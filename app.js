4f3e815aa439a7d2edda6627adb02e779509b59f
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2020-09-24 17:57:27 +0530

    data binding

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 new Vue({
-    el:'#vue-app',
-    data:{
-        name:'Anuradha',
-        job:'Engineer'
+    el: '#vue-app',
+    data: {
+        name: 'Anuradha',
+        job: 'Engineer',
+        website:'https://www.youtube.com/'
+    },
+    methods: {
+        greet: function (word) {
+            return 'Hello world' + ' ' + word + ' ' + this.name
+        }
     }
+
 });
