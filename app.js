a113120db066d783b73ae69e9f599d94b1cfc435
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2020-09-24 19:23:35 +0530

    refs

--- app.js
+++ app.js
@@ -3,32 +3,37 @@ new Vue({
     data: {
         name: 'Anuradha',
         job: 'Development',
-        website:'https://www.youtube.com/',
-        age:25,
-        a:0,
-        b:0,
-        error:false,
-        success:false,
-        characters:['Spiderman','Batman','Superman'],
-        pawns:[
-            {name:'Kavinda',age:23},
-            {name:'Usitha',age:24},
-            {name:'Vishwa',age:21},
-        ]
+        website: 'https://www.youtube.com/',
+        age: 25,
+        a: 0,
+        b: 0,
+        error: false,
+        success: false,
+        characters: ['Spiderman', 'Batman', 'Superman'],
+        pawns: [
+            {name: 'Kavinda', age: 23},
+            {name: 'Usitha', age: 24},
+            {name: 'Vishwa', age: 21},
+        ],
+        output: 'Your favourite superhero'
     },
     methods: {
         greet: function (word) {
             return 'Hello world' + ' ' + word + ' ' + this.name
         },
-        add:function (x){
+        add: function (x) {
             this.age += x;
+        },
+        readRefs: function () {
+            console.log(this.$refs.input.value)
+            this.output=this.$refs.input.value
         }
     },
-    computed:{
-        addToA: function (){
+    computed: {
+        addToA: function () {
             return this.a + this.age
         },
-        addToB: function (){
+        addToB: function () {
             return this.b + this.age
         }
     }
